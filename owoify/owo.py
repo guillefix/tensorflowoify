a6fdddcce43195cd6cfb228fc168fc3272d97cff
Guillermo Valle <guillefix@gmail.com>
2020-11-11 00:01:46 +0000

    uwu

--- owoify/owo.py
+++ owoify/owo.py
@@ -10,8 +10,23 @@ class BaseOwoifator(ABC):
     def owoify(self, text: str) -> str:
         pass
 
-
 class Owoifator(BaseOwoifator):
+    _special_patterns = {
+        "math":"mawth",
+        "nn":"nwn",
+        "logits":"lowogits",
+        "softmax":"sowftmax",
+        "bitwise_or":"bitwise_owo",
+        "raw_ops":"rawr_oops",
+        "foldr":"fowdr",
+        "foldl":"fowdl",
+        "Dot":"Boop",
+        "dot":"boop",
+        "Dense":"Thicc",
+        "dense":"thicc",
+        "loss$":"iIiIIL",
+        "Loss$":"IIiIIL",
+    }
     _patterns = {
         r"[lr]": "w",
         r"[LR]": "W",
@@ -24,8 +39,17 @@ class Owoifator(BaseOwoifator):
         r"!+": lambda _: " " + random.choice(kaomoji),
     }
 
+
+
     def owoify(self, text: str) -> str:
+        special_patterns = list(enumerate(self._special_patterns.items()))
+        for i, (pattern, repl) in special_patterns:
+            text = re.sub(pattern, chr(i), text)
+
         for pattern, repl in self._patterns.items():
             text = re.sub(pattern, repl, text)
+
+        for i, (pattern, repl) in special_patterns:
+            text = re.sub(chr(i), repl, text)
 
         return text
---
+++ sandbox.py
@@ -0,0 +1,8 @@
+from owoify import Owoifator
+
+owoifator = Owoifator()
+
+owoifator.owoify("foldr")  # Hewwo fwiend (*^ω^)
+owoifator.owoify("foldl")  # Hewwo fwiend (*^ω^)
+
+#Also remember to make an alias for MLP which is MyLittlePony
